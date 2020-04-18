bdf4f69ece24f77b9f4ed5fc6089d6355a5be05a
Emily Richter <emrichter1996@gmail.com>
2020-04-18 14:41:31 -0500

    Modifying for Heroku deployment

--- ems/app.js
+++ ems/app.js
@@ -164,8 +164,8 @@ app.get('/view/:queryName', function(req, res) {
 
 
 // Create new Node server and listens local on port 8080
-http.createServer(app).listen(8080, function() {
-  console.log("Application started on port 8080!");
+http.createServer(app).listen(app.get("port"), function() {
+  console.log("Application started on " + app.get("port") + "!");
 });
 
 // End program
